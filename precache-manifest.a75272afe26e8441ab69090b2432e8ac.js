4cd8f01e452df37ebaaac94194f3f7613a78de1b
Vince Lauffer <vlauffer@gmail.com>
2019-12-09 14:29:30 -0500

    Updates

--- precache-manifest.a75272afe26e8441ab69090b2432e8ac.js
+++ precache-manifest.a75272afe26e8441ab69090b2432e8ac.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ad498fde4a1b23ffc09ad792fa6945e9",
+    "revision": "e4f1a747fd1ca0775984a4aa58e20c79",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "b2ef440d0d30ce5eab83",
+    "revision": "b5a3377f5d28cd3400b8",
     "url": "/static/css/main.5b1143dd.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.36177aea.chunk.js.LICENSE"
   },
   {
-    "revision": "b2ef440d0d30ce5eab83",
-    "url": "/static/js/main.0d25fbd5.chunk.js"
+    "revision": "b5a3377f5d28cd3400b8",
+    "url": "/static/js/main.9c5b5b4a.chunk.js"
   },
   {
     "revision": "c9913191e565de632244",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3ad6976d2376ffb2bf664e2874ed86d.js"
+  "/precache-manifest.a75272afe26e8441ab69090b2432e8ac.js"
 );
 
 self.addEventListener('message', (event) => {
